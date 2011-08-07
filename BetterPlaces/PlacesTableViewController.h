56cc2d69d142adb091d5668f549f09dd265892b7
chrismoyer <chris.moyer@gmail.com>
2011-08-07 00:50:18 -0400

    some files I missed

---
+++ BetterPlaces/PlacesTableViewController.h
@@ -0,0 +1,20 @@
+//
+//  PlacesTableViewController.h
+//  FlickrPickr
+//
+//  Created by Chris Moyer on 7/25/11.
+//  Copyright 2011 MoeCode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlacesTableViewController : UITableViewController
+{
+    NSDictionary *places;
+    NSArray *placeSections;
+    NSManagedObjectContext *context;
+}
+
+- initInManagedObjectContext:(NSManagedObjectContext *)context;
+
+@end
